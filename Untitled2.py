b39adffbb926c0e0c06731898c45928ef082f9cb
vickyvea <114747075+vickyvea@users.noreply.github.com>
2022-10-09 05:14:30 +0100

    Add files via upload

---
+++ Untitled2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # My Jupyter Notebook on IBM Watson Studio
+
+# **My name is Victoria Ijemuah**
+# 
+# I would love to  be a data scientist
+
+# _I am interested in data science because i would love to apply data science
+# in modern day technology, to build models_
+
+# ### In the cell below, we are to multiply 12 by 10 and print out the result.
+
+# In[1]:
+
+
+a = 12 * 10
+print(a)
+
+
+# [this is a link to google](http://www.google.com)
+# 
+# ![a beautiful flower](https://dr282zn36sxxg.cloudfront.net/datastreams/f-d%3A95e12282801a413841321c5f8c154b867ac1106bec4695fc5a32b782%2BIMAGE_THUMB_POSTCARD_TINY%2BIMAGE_THUMB_POSTCARD_TINY.1)
+# 
+# 
+# * Banana
+# * Apple
+# * Orange
+# * Pineapple
+# * Pawpaw
+# * Watermelon
+# 
+# 
+
+# In[ ]:
+
+
+
+
